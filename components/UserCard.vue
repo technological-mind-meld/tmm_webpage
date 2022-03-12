c057fc465fd40b61dd76209dab5952af692f38d7
Mücahit Nezir <mucahitnezir@gmail.com>
2022-03-12 18:50:24 +0300

    Show social media urls on mouse hover

--- components/UserCard.vue
+++ components/UserCard.vue
@@ -1,19 +1,24 @@
 <template>
-  <v-card width="50%" class="text-center" tile elevation="0">
-    <v-img :src="photoURL" :alt="fullName" />
-    <div class="text-subtitle-1" v-text="fullName" />
-    <div class="d-flex">
-      <v-btn v-if="linkedinURL" icon :href="linkedinURL" target="_blank" color="#0077b5">
-        <v-icon>{{ icons.mdiLinkedin }}</v-icon>
-      </v-btn>
-      <v-btn v-if="personalURL" icon :href="personalURL" target="_blank" color="#666">
-        <v-icon>{{ icons.mdiWeb }}</v-icon>
-      </v-btn>
-      <v-btn v-if="githubURL" icon :href="githubURL" target="_blank" color="#333">
-        <v-icon>{{ icons.mdiGithub }}</v-icon>
-      </v-btn>
-    </div>
-  </v-card>
+  <v-hover v-slot="{ hover }">
+    <v-card width="60%" class="text-center" tile elevation="0">
+      <v-img :src="photoURL" :alt="fullName">
+        <v-expand-transition>
+          <div v-if="hover" class="d-flex align-center justify-center social-wrapper">
+            <v-btn v-if="linkedinURL" icon :href="linkedinURL" target="_blank" color="primary">
+              <v-icon>{{ icons.mdiLinkedin }}</v-icon>
+            </v-btn>
+            <v-btn v-if="personalURL" icon :href="personalURL" target="_blank" color="#333">
+              <v-icon>{{ icons.mdiWeb }}</v-icon>
+            </v-btn>
+            <v-btn v-if="githubURL" icon :href="githubURL" target="_blank" color="#333">
+              <v-icon>{{ icons.mdiGithub }}</v-icon>
+            </v-btn>
+          </div>
+        </v-expand-transition>
+      </v-img>
+      <div class="text-subtitle-1" v-text="fullName" />
+    </v-card>
+  </v-hover>
 </template>
 
 <script>
@@ -50,3 +55,10 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.social-wrapper {
+  background-color: rgb(100 100 100 / 40%);
+  height: 100%;
+}
+</style>
