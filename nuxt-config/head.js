f99bf6bdc08b14fa581ef7b19bff40165c74b22f
Mücahit Nezir <mucahitnezir@gmail.com>
2022-03-12 16:21:55 +0300

    Update favicons

--- nuxt-config/head.js
+++ nuxt-config/head.js
@@ -28,6 +28,37 @@ export const head = {
     { hid: 'og:image:alt', property: 'og:image:alt', content: headTags.siteName }
   ],
   link: [
-    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    {
+      name: 'apple-mobile-web-app-title',
+      content: '0x4447 LLC'
+    },
+    {
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicons/favicon-32x32.png'
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicons/favicon-16x16.png'
+    },
+    {
+      name: 'apple-touch-icon',
+      href: '/favicons/apple-touch-icon.png'
+    },
+    {
+      rel: 'manifest',
+      href: '/favicons/site.webmanifest'
+    },
+    {
+      rel: 'mask-icon',
+      href: 'favicons/safari-pinned-tab.svg',
+      color: '#5bbad5'
+    }
   ]
 }
