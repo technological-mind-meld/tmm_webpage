1a004160d120fe8bc2c6b21ba84ec45cf563f1e5
Mücahit Nezir <mucahitnezir@gmail.com>
2022-03-13 15:55:55 +0300

    Update style of banner

--- components/TheBanner.vue
+++ components/TheBanner.vue
@@ -3,14 +3,25 @@
     <v-col cols="12" md="7">
       <div class="banner-description">
         <!-- Main Image -->
-        <v-img src="/users/david.jpg" max-width="45%" contain />
+        <v-img src="/users/david.jpg" lazy-src="/users/david.jpg" max-width="45%" contain>
+          <template #placeholder>
+            <v-row class="fill-height" align="center" justify="center" no-gutters>
+              <v-progress-circular indeterminate color="grey lighten-5" />
+            </v-row>
+          </template>
+        </v-img>
 
         <!-- Description Text -->
-        <p class="text-subtitle-1">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias laboriosam, molestias obcaecati perspiciatis quis repellat vero voluptates. Ad, consectetur consequuntur dolore eaque, facilis fuga magni perspiciatis quo vero voluptate voluptatibus.
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias laboriosam, molestias obcaecati perspiciatis quis repellat vero voluptates. Ad, consectetur consequuntur dolore eaque, facilis fuga magni perspiciatis quo vero voluptate voluptatibus.
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias laboriosam, molestias obcaecati perspiciatis quis repellat vero voluptates. Ad, consectetur consequuntur dolore eaque, facilis fuga magni perspiciatis quo vero voluptate voluptatibus.
-        </p>
+        <div>
+          <h2 class="pt-5">
+            Memory dump podcast
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias laboriosam, molestias obcaecati perspiciatis quis repellat vero voluptates. Ad, consectetur consequuntur dolore eaque, facilis fuga magni perspiciatis quo vero voluptate voluptatibus.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias laboriosam, molestias obcaecati perspiciatis quis repellat vero voluptates. Ad, consectetur consequuntur dolore eaque, facilis fuga magni perspiciatis quo vero voluptate voluptatibus.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias laboriosam, molestias obcaecati perspiciatis quis repellat vero voluptates. Ad, consectetur consequuntur dolore eaque, facilis fuga magni perspiciatis quo vero voluptate voluptatibus.
+          </p>
+        </div>
       </div>
     </v-col>
     <v-col cols="12" md="5">
@@ -30,8 +41,13 @@ export default {
 .banner-description {
   .v-image {
     float: left;
-    margin: 0 25px 15px 0;
+    margin: 0 30px 15px 0;
     border-radius: 0.375rem;
+  }
+
+  p {
+    font-size: 1.1875rem;
+    color: $text-gray-700;
   }
 }
 </style>
