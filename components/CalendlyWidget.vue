7a9664543d508e3df69182f27d06a78aaa6992af
Mücahit Nezir <mucahit.nezir@mosstar.com.tr>
2022-03-14 17:12:46 +0300

    Load Calendly widget with iframe

--- components/CalendlyWidget.vue
+++ components/CalendlyWidget.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
-    <!-- Calendly inline widget begin -->
-    <div class="calendly-inline-widget" data-url="https://calendly.com/0x4447/podcast?hide_event_type_details=1&hide_gdpr_banner=1&text_color=27303b&primary_color=007bff" style="min-width: 320px; height: 630px;" />
-    <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async />
-    <!-- Calendly inline widget end -->
-  </div>
+  <iframe
+    class="iframe-calendly"
+    src="https://calendly.com/0x4447/podcast?hide_event_type_details=1&hide_gdpr_banner=1&text_color=27303b&primary_color=007bff"
+    width="100%"
+    height="480"
+  />
 </template>
 
 <script>
@@ -12,3 +12,12 @@ export default {
   name: 'CalendlyWidget'
 }
 </script>
+
+<style scoped>
+iframe.iframe-calendly {
+  height: 480px;
+  overflow: hidden;
+  border: 0;
+  border-radius: 0.375rem;
+}
+</style>
