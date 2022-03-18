4d5a406e947cc3c796e7b967d695f7499a979ab2
Mücahit Nezir <mucahitnezir@gmail.com>
2022-03-18 20:36:55 +0300

    Fix #10

--- components/UserAvatarWithName.vue
+++ components/UserAvatarWithName.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="d-flex flex-column align-center">
     <UserAvatar :user="user" :size="width" />
-    <div class="text-subtitle-1" v-text="fullName" />
+    <div class="text-subtitle-1" v-text="name" />
   </div>
 </template>
 
@@ -11,6 +11,12 @@ import userCardMixin from '~/mixins/userCardMixin'
 export default {
   name: 'UserAvatarWithName',
   mixins: [userCardMixin],
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  },
   data () {
     return {
       width: 140
